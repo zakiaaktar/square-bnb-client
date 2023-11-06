ab4ec05a78c9a8de64f821244bfb521104ec4596
Zakia Aktar <zakiaaktar@Zakias-MacBook-Pro.local>
2023-11-06 11:08:07 +0100

    basic layout and route done

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom"
+import MainLayout from "./components/layout/MainLayout"
+
+function App() {
+  
+
+  return (
+    <>
+       <MainLayout>
+          <Outlet></Outlet>
+       </MainLayout> 
+    </>
+  )
+}
+
+export default App
